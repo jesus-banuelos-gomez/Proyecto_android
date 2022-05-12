b5bde09dba1ed7d8ac9a889d3feea91cd41b893b
jesus bañuelos <jesusbg1225dic@gmail.com>
2022-05-11 19:07:15 -0500

    conecntando api

--- app/src/main/java/com/example/proyecto_android/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_android/MainActivity.kt
@@ -1,14 +1,41 @@
 package com.example.proyecto_android
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.textclassifier.TextSelection
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
     }
+}
+
+fun getJuegos(){
+
+    val queue = Volley.newRequestQueue(this)
+    val url = "https://rickandmortyapi.com/api/character"
+
+    val objectRequest = JsonObjectRequest(
+        Request.Method.GET,
+        url,
+        null,
+        Response.Listener { response ->
+            val juegoJson = response.getJSONObject(indice)
+        },
+
+        Response.ErrorListener {
+            
+        }
+
+    )
+
 }
